7032d2c54fc54f1042fd24da43a7247fd3ba69b2
Mahlet-assefa <mahletassefacodes@gmail.com>
2025-12-24 00:00:48 +0300

    Base: Foundation

---
+++ src/server/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const fs = require('fs');
+const path = require('path');
+const LOG_FILE = path.join(__dirname, '../../server.log');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    const msg = `REQ: ${req.method} ${req.url}\n`;
+    fs.appendFileSync(LOG_FILE, msg);
+    console.log(msg.trim());
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.send('Event Registration System API');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
